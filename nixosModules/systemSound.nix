6d8182c128099d8d2f5ad64400ae9d384e163a44
webermael <webermael7@gmail.com>
2024-10-06 00:54:25 +0200

    update sound

--- nixosModules/systemSound.nix
+++ nixosModules/systemSound.nix
@@ -6,15 +6,8 @@
   };
 
   config = lib.mkIf config.systemSound.enable {
-    sound.enable = true;
+    hardware.pulseaudio.enable = true;
+    services.pipewire.audio.enable = true;
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-    };
-
   };
 }
