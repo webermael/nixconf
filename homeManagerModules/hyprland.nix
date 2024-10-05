af060abb12133e5ff879d1056a044a41abba46e7
webermael <webermael7@gmail.com>
2024-10-06 00:25:18 +0200

    add temporary hyprland config

--- homeManagerModules/hyprland.nix
+++ homeManagerModules/hyprland.nix
@@ -6,10 +6,35 @@
   };
 
   config = lib.mkIf config.hyprland.enable {
+    programs.firefox.enable = true;
+    programs.kitty.enable = true;
+    
     wayland.windowManager.hyprland = {
       enable = true;
       settings = {
-        "mod" = "SUPER";
+        "mod" = "SUPER";        
+        bind = [
+          "$mod, return, exec, kitty"
+          "$mod, B, exec, firefox"
+          "$mod, SHIFT, Q, killactive,"
+          "$mod SHIFT, M, exit,"
+          "$mod, F, fullscreen,"
+
+          "$mod, left, movefocus, l"
+          "$mod, right, movefocus, r"
+          "$mod, up, movefocus, u"
+          "$mod, down, movefocus, d"
+
+          "$mod, h, movefocus, l"
+          "$mod, l, movefocus, r"
+          "$mod, k, movefocus, u"
+          "$mod, j, movefocus, d"
+
+          "$mod SHIFT, h, movewindow, l"
+          "$mod SHIFT, l, movewindow, r"
+          "$mod SHIFT, k, movewindow, u"
+          "$mod SHIFT, j, movewindow, d"
+        ]       
       };
     };
 
