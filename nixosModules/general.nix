54e6e21f1641da2a29461531fb311df596259c2c
webermael <webermael7@gmail.com>
2024-10-05 18:46:00 +0200

    add module general

---
+++ nixosModules/general.nix
@@ -0,0 +1,32 @@
+{ pkgs, lib, config ...}: {
+
+  options = {
+    general.enable =
+      lib.mkEnableOption "general settings";
+  };
+
+  config = lib.mkIf config.general.enable {
+    nixpkgs = {
+      config = {
+        allowUnfree = true;
+      };
+    };
+
+    nix.settings = {
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;
+    };
+
+    time.timeZone = "Europe/Zurich";
+    console.keyMap = "us-acentos";
+
+    environment.systemPackages = with pkgs; [
+      pavucontol
+      neovim
+      tldr
+      wget
+      git
+      gh
+    ];
+  };
+}
